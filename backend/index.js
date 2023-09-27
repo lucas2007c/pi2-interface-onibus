b9cf69c26241cf71243309adde4e8f9a01d18471
lucas2007c <carneiro.g@aluno.ifsp.edu.br>
2023-09-26 21:56:25 -0300

    add usuario routes

--- backend/index.js
+++ backend/index.js
@@ -129,7 +129,43 @@ app.delete('/linha/:id', async (req, res) => {
     res.status(200).json(query)
 }); // DELETAR
 
+// usuario ------------------------------------------------------------------------------------------------
+app.get('/usuario', async (req, res) => {
+    const [query] = await connection.execute('select * from usuario');
+    res.status(200).json(query);
+}); // GERAL
 
+app.get('/usuario/:id', async (req, res) => {
+    const { id } = req.params
+    const [query] = await connection.execute('select * from usuario where id = ?', [id]);
+    res.status(200).json(query);
+}); // POR ID
+
+app.get('/usuario/busca/:nomex', async (req, res) => {
+    const { nomex } = req.params;
+    const nome = '%' + nomex + '%';
+    const [query] = await connection.execute('select * from usuario where nome like ?', [nome]);
+    res.status(200).json(query);
+}); // LIKE
+
+app.post('/usuario', async (req, res) => {
+    const { nome, email } = req.body
+    const [query] = await connection.execute('insert into usuario (nome, email) values (?,?)', [nome, email]);
+    res.status(201).json(query);
+}); // CADASTRAR
+
+app.put('/usuario/:id', async (req, res) => {
+    const { id } = req.params
+    const { nome, email } = req.body
+    const [query] = await connection.execute('update usuario set nome = ?, email = ? where id = ?', [nome, email, id]);
+    res.status(200).json(query);
+}); // EDITAR
+
+app.delete('/usuario/:id', async (req, res) => {
+    const { id } = req.params
+    const [query] = await connection.execute("delete from usuario where id  = ?", [id])
+    res.status(200).json(query)
+}); // DELETAR
 
 // RODANDO SERVIDOR
 app.listen(port, ()=>{
