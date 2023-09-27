51e12188521790ef5e895cfaba94f5945340cf2a
lucas2007c <carneiro.g@aluno.ifsp.edu.br>
2023-09-26 21:53:27 -0300

    add .jsons and passageiro routes

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+const port = 3000;
+
+const app = express();
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    database: 'fasttravel',
+    user: 'root',
+    password: ''
+});
+
+
+// PASSAGEIRO --------------------------------------------------------------------------------
+app.get('/passageiro', async (req, res) =>{
+    const [query] = await connection.execute('select * from passageiro');
+    res.status(200).json(query);
+}); // GERAL
+
+app.get('/passageiro/:id', async (req, res) => {
+    const {id} = req.params
+    const [query] = await connection.execute('select * from passageiro where id = ?', [id]);
+    res.status(200).json(query);
+}); // POR ID
+
+app.get('/passageiro/busca/:nomex', async (req, res) => {
+    const {nomex} = req.params;
+    const nome = '%' +nomex+ '%';
+    const [query] = await connection.execute('select * from passageiro where nome like ?', [nome]);
+    res.status(200).json(query);
+}); // LIKE
+
+app.post('/passageiro', async (req, res) => {
+    const {nome, email} = req.body
+    const [query] = await connection.execute('insert into passageiro (nome, email) values (?,?)', [nome, email]);
+    res.status(201).json(query);
+}); // CADASTRAR
+
+app.put('/passageiro/:id', async (req, res) => {
+    const {id} = req.params
+    const {nome, email} = req.body
+    const [query] = await connection.execute('update passageiro set nome = ?, email = ? where id = ?', [nome, email, id]);
+    res.status(200).json(query);
+}); // EDITAR
+
+app.delete('/passageiro/:id', async (req, res) => {
+    const {id} = req.params
+    const [query] = await connection.execute("delete from passageiro where id  = ?", [id])
+    res.status(200).json(query)
+}); // DELETAR
+
+
+
+// RODANDO SERVIDOR
+app.listen(port, ()=>{
+    console.log(`servidor rodando na porta ${port}`)
+});
