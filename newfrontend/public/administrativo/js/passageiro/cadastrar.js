5b16e27cb3755085a454587c3281d3c316dc4cf6
manuela <manuelazotavio@gmail.com>
2023-10-29 17:44:04 -0300

    add function formatarCPF and formatarNumero

--- newfrontend/public/administrativo/js/passageiro/cadastrar.js
+++ newfrontend/public/administrativo/js/passageiro/cadastrar.js
@@ -63,3 +63,24 @@ function updateCPF() {
 // Adicione um ouvinte de eventos para chamar a função updateCPF quando o campo é alterado
 const cpfInput = document.getElementById("cpf");
 cpfInput.addEventListener("input", updateCPF);
+
+// Função para formatar o numero de celular
+function formatarNumero(numero) {
+  // Remove qualquer caractere que não seja um dígito
+  numero = numero.replace(/\D/g, "");
+
+  // Adiciona os parenteses e o traço
+  numero = numero.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+
+  return numero;
+}
+
+// Função para atualizar o valor do campo de numero de celular formatado
+function updateNumero() {
+  const numeroInput = document.getElementById("numero");
+  numeroInput.value = formatarNumero(numeroInput.value);
+}
+
+// Adicione um ouvinte de eventos para chamar a função updateNumero quando o campo é alterado
+const numeroInput = document.getElementById("numero");
+numeroInput.addEventListener("input", updateNumero);
