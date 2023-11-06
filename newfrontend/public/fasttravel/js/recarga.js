9d0390c73bc218a078058f2f6a3831eb04ee2b1a
IFSPRenan <alves.renan1@aluno.ifsp.edu.br>
2023-11-06 18:09:49 -0300

    change recarga

---
+++ newfrontend/public/fasttravel/js/recarga.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#form-recarga");
+
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    if (form.checkValidity()) {
+      const cpf = document.querySelector("#cpfRecarga").value;
+      const recarga = document.querySelector("#valorRecarga").value;
+
+      const data = {
+        cpf,
+        recarga,
+      };
+
+      try {
+        const response = await axios.patch(
+          "http://localhost:3000/recarga/",
+          data
+        );
+
+        console.log("success", "Cadastro realizado sucesso");
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+
+    form.classList.add("was-validated");
+  });
+});
