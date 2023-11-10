5668ef6b6bb8bcfc0fba45202c5c3a420b190d88
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2023-11-10 14:35:29 -0300

    add formatacao cpf recarga

---
+++ frontend/public/fasttravel/js/formatarCpf.js
@@ -0,0 +1,23 @@
+// CPF
+
+// Função para formatar o CPF
+function formatarCPF(cpf) {
+    // Remove qualquer caractere que não seja um dígito
+    cpf = cpf.replace(/\D/g, "");
+  
+    // Adiciona os pontos e o traço
+    cpf = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+  
+    return cpf;
+  }
+  
+  // Função para atualizar o valor do campo de CPF formatado
+  function updateCPF() {
+    const cpfInput = document.getElementById("cpf");
+    cpfInput.value = formatarCPF(cpfInput.value);
+  }
+  
+  // Adicione um ouvinte de eventos para chamar a função updateCPF quando o campo é alterado
+  const cpfInput = document.getElementById("cpf");
+  cpfInput.addEventListener("input", updateCPF);
+  
