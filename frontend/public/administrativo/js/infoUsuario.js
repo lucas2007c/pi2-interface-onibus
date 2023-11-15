b663b16421fbf680afb0267e61fa51527c25316c
IFSPRenan <alves.renan1@aluno.ifsp.edu.br>
2023-11-15 02:20:01 -0300

    add infoUsuario in admin index

---
+++ frontend/public/administrativo/js/infoUsuario.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", async (event) => {
+  const id = await verifyToken();
+
+  const UsuarioId = id;
+
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/usuario/${UsuarioId}`
+    );
+    const usuario = response.data;
+
+    document.querySelector(
+      "#fotoUsuario"
+    ).src = `http://localhost:3000/${usuario.foto_caminho}`;
+    document.querySelector("#nomeUsuario").textContent = usuario.nome;
+    document.querySelector("#emailUsuario").textContent = usuario.email;
+  } catch (error) {
+    console.error("Erro ao buscar os dados:", error);
+  }
+});
