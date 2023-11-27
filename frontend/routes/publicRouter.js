77dc62149df989add04d6e472bdb62ae17c4148b
IFSPRenan <alves.renan1@aluno.ifsp.edu.br>
2023-11-27 01:29:03 -0300

    add page cartao + starting creation of other pages

--- frontend/routes/publicRouter.js
+++ frontend/routes/publicRouter.js
@@ -7,12 +7,24 @@ router.get('/', (req, res) => {
   res.render('fasttravel/index'); // Exemplo de renderização de uma página
 });
 
-router.get('/recarga', (req, res) => {
-  res.render('fasttravel/recarga');
+router.get('/cartao', (req, res) => {
+  res.render('fasttravel/cartao');
 });
 
 router.get('/linhas', (req, res) => {
   res.render('fasttravel/linhas');
+});
+
+router.get('/recarga', (req, res) => {
+  res.render('fasttravel/recarga');
+});
+
+router.get('/saldo', (req, res) => {
+  res.render('fasttravel/saldo');
+});
+
+router.get('/embarques', (req, res) => {
+  res.render('fasttravel/embarques');
 });
 
 module.exports = router;
